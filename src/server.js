5c841c0831f4617dc9adf27f1e84e0c4526d0a93
Santiago Torres <Santiago.Torres1407@gmail.com>
2024-04-05 16:29:46 -0400

    first commit of backend. Created backend separately

---
+++ src/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: 'db-shalom.cds6c28ae9a0.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Concentrix*2022*',
+  database: 'db_shalom',
+});
+
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database:', error);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
+app.get('/api/products', (req, res) => {
+  connection.query('SELECT * FROM products', (error, results) => {
+    if (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.post('/api/signup', (req, res) => {
+  const { firstName, lastName, email, phoneNumber, password, confirmPassword } = req.body;
+  const sql = 'INSERT INTO customers (first_name, last_name, email, phone, password_hash) VALUES (?, ?, ?, ?, ?)'
+  const values = [firstName, lastName, email, phoneNumber, password]
+  connection.query(sql, values, (error, results) => {
+    if (error) {
+      console.error('Error creating account:', error);
+      res.status(500).json({error: 'Error creating account'});
+    } else {
+      console.log('Account created successfully');
+      res.status(200).json({ message: 'Account created successfully'});
+    }
+  })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
