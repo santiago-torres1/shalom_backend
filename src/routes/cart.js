88b1ae33c5f9f5518949a409f8b1e59e97ae5ac9
santiago-torres1 <Santiago.torres1407@gmail.com>
2024-04-09 18:45:19 -0400

    modified delete route to be patch because a body is needed

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -10,37 +10,32 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const { itemId, quantity } = req.body;
-    if (!req.cookies.cartShalom) {
-        res.cookie('cartShalom', JSON.stringify({ items: [{ itemId, quantity }] }), { maxAge: SEVEN_DAYS, httpOnly: true });
-        res.status(200).json({ message: 'Cart created successfully' });
-    }
     const cart = req.cookies.cartShalom;
     const existingItemIndex = cart.items.findIndex(item => item.itemId === itemId);
+    console.log(req.body);
     if (existingItemIndex !== -1) {
-        cart.items[existingItemIndex].quantity += quantity;
+        cart.items[existingItemIndex].quantity += 1;
     } else {
         cart.items.push({ itemId, quantity });
     }
     res.cookie('cartShalom', cart, { maxAge: SEVEN_DAYS, httpOnly: false });
-    res.status(200).json({message: 'Item added to cart successfully'});
+    return res.status(200).json({message: 'Item added to cart successfully'});
 });
 
-router.delete('/', (req, res) => {
+router.patch('/', (req, res) => {
     const { itemId, quantity } = req.body;
-    if (!req.cookies.cartShalom) {
-        res.status(404).json({ message: 'Cart not found' });
-    }
     const cart = req.cookies.cartShalom;
     const existingItemIndex = cart.items.findIndex(item => item.itemId === itemId);
+    console.log(req.body);
     if (existingItemIndex !== -1) {
-        cart.items[existingItemIndex].quantity -= quantity;
+        cart.items[existingItemIndex].quantity -= 1;
         if (cart.items[existingItemIndex].quantity <= 0) {
             cart.items.splice(existingItemIndex, 1);
         }
         res.cookie('cartShalom', cart, { maxAge: SEVEN_DAYS, httpOnly: false });
-        res.status(200).json({message: 'Item deleted from cart successfully'});
+        return res.status(200).json({message: 'Item deleted from cart successfully'});
     }
-    res.status(404).json({ message: 'Item not found in cart' });
+    return res.status(404).json({ message: 'Item not found in cart' });
 });
 
 module.exports = router;
