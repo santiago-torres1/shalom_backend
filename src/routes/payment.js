bc511c300aac8e3df715eb4a6e6750099696f46f
Santiago Torres <Santiago.Torres1407@gmail.com>
2024-04-19 11:11:07 -0400

    Added response code for post method

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const SEVEN_DAYS = 7 * 24 * 60 * 60 * 1000;
 
 router.get('/', (req, res) => {
-    console.log(req);
     res.redirect('/')
 });
 
 router.post('/', (req, res) => {
     console.log(req);
+    res.status(200).send('data received')
 });
 
 module.exports = router;
